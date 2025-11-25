287ed683951e02f55bba814585e725d014ea74d5
kans772 <kk6046114@gmail.com>
2025-11-25 12:46:46 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,8 +41,9 @@ export default function App() {
           }
         />
         <Route path="*" element={<Welcome/>} />
+        <Footer/>
       </Routes>
-      <Footer/>
+      
     </Router>
   );
 }
